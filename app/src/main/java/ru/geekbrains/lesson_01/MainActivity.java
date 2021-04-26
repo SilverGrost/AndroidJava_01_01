a674556ed52b1234219c051fd567aea862605234
SilverGrost <silvergrost@silcomsoft.ru>
2021-04-26 10:41:45 +0300

    ДЗ №1

--- app/src/main/java/ru/geekbrains/lesson_01/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lesson_01/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -18,106 +17,84 @@ import android.widget.ToggleButton;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
 
-    //EditText edit_1;
-    //TextView textView;
-    //Switch switch1;
+    TextView textView;
+    EditText edit1;
+    Button button;
+    Switch switch1;
+    CheckBox checkBox;
+    ToggleButton toggleButton;
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
+        edit1 = findViewById(R.id.edit1);
+        button = findViewById(R.id.button);
+        switch1 = findViewById(R.id.switch1);
+        checkBox = findViewById(R.id.checkBox);
+        toggleButton = findViewById(R.id.toggleButton);
+        imageButton = findViewById(R.id.imageButton);
 
-        EditText edit1 = findViewById(R.id.edit1);
-        final EditText editText1 = findViewById(R.id.edit1);
-        final TextView textView = findViewById(R.id.textView);
+
+        edit1.setOnClickListener(this);
+        button.setOnClickListener(this);
+        switch1.setOnClickListener(this);
+        checkBox.setOnClickListener(this);
+        toggleButton.setOnClickListener(this);
+        imageButton.setOnClickListener(this);
+
+        switch1.setOnCheckedChangeListener(this);
+        checkBox.setOnCheckedChangeListener(this);
+
 
         SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
         SimpleDateFormat sdfDate = new SimpleDateFormat("dd-EEEE-yyyy");
         String time = sdfTime.format(Calendar.getInstance().getTime());
         String date = sdfDate.format(Calendar.getInstance().getTime());
-        textView.setText(time);
-        editText1.setText(date);
-
-        CheckBox checkBox = findViewById(R.id.checkBox);
-        ToggleButton toggleButton = findViewById(R.id.toggleButton);
-        Button button = findViewById(R.id.button);
-        Switch switch1 = findViewById(R.id.switch1);
-        ImageButton imageButton = findViewById(R.id.imageButton);
-
-
-
-
-        edit1.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                textView.setText(edit1.getText());
-                return true;
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch1.setChecked(!switch1.isChecked());
-            }
-        });
-
-        switch1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkBox.setChecked(!checkBox.isChecked());
-            }
-        });
-
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                checkBox.setChecked(isChecked);
-            }
-        });
-
-        checkBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleButton.setChecked(!toggleButton.isChecked());
-            }
-        });
-
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                toggleButton.setChecked(isChecked);
-            }
-        });
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Intent intent = new Intent(this, MainActivity2.class);
-                //setContentView(R.layout.activity_main2);
-
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            }
-        });
-
+        textView.setText(date + ' ' + time);
     }
 
 
-    /*@Override
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.edit1:
-                textView.setText(edit_1.getText());
+                textView.setText(edit1.getText());
                 break;
             case R.id.button:
                 switch1.setChecked(!switch1.isChecked());
                 break;
+            case R.id.switch1:
+                checkBox.setChecked(!checkBox.isChecked());
+                break;
+            case R.id.checkBox:
+                toggleButton.setChecked(!toggleButton.isChecked());
+                break;
+            case R.id.imageButton:
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                break;
             default:
                 break;
         }
-    }*/
+    }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.switch1:
+                checkBox.setChecked(isChecked);
+                break;
+            case R.id.checkBox:
+                toggleButton.setChecked(isChecked);
+                break;
+            default:
+                break;
+        }
+    }
 }
--- app/src/main/java/ru/geekbrains/lesson_01/MainActivity2.java
+++ app/src/main/java/ru/geekbrains/lesson_01/MainActivity2.java
@@ -1,33 +1,49 @@
 package ru.geekbrains.lesson_01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements CalendarView.OnDateChangeListener {
+
+    EditText editTextDate;
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        //ImageButton imageButton = findViewById(R.id.imageButton3);
-        //TextView textView2 = findViewById(R.id.textView);
+        editTextDate = findViewById(R.id.editTextDate);
+        calendarView = findViewById(R.id.calendarView2);
 
-        //SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
-        //SimpleDateFormat sdfDate = new SimpleDateFormat("dd-EEEE-yyyy");
-        //String time = sdfTime.format(Calendar.getInstance().getTime());
-        //String date = sdfDate.format(Calendar.getInstance().getTime());
-        //textView2.setText(time);
+        calendarView.setOnDateChangeListener(this);
 
+        SimpleDateFormat sdfDate = new SimpleDateFormat("dd-EEEE-yyyy");
+        String date = sdfDate.format(calendarView.getDate());
+        editTextDate.setText(date);
+    }
 
+    @Override
+    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+        int mYear = year;
+        int mMonth = month;
+        int mDay = dayOfMonth;
+        Calendar calendar = Calendar.getInstance();
 
+        calendar.set(year, month, dayOfMonth);
+        SimpleDateFormat sdfDate = new SimpleDateFormat("dd-EEEE-yyyy");
+        String date = sdfDate.format(calendar.getTime());
+        editTextDate.setText(date);
     }
 }
